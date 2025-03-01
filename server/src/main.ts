0ab2cda0a108e88340f074e2571609ab4df294b4
GrzegorzBanaszak <grzegorz.banaszak95@gmail.com>
2025-03-01 12:32:53 +0100

    Server - dodanie dokumentacji swagger

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Farm Management Dashboard API')
+    .setDescription('The Farm Management Dashboard API description')
+    .setVersion('1.0')
+    .addTag('Farm Management Dashboard')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
