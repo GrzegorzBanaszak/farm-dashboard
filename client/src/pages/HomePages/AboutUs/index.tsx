e95fb0846b1b989863c210edc3b6881d5eec0559
SaI0I0 <98886546+SaI0I0@users.noreply.github.com>
2025-03-26 21:20:59 +0100

    Przerobienie pliku HTML na plik TSX
    Przerobienie pliku HTML na plik TSX, plik HTML zostaje do celów testowych. Przeniesienie obrazków do folderu "public".

--- client/src/pages/HomePages/AboutUs/index.tsx
+++ client/src/pages/HomePages/AboutUs/index.tsx
@@ -1,5 +1,33 @@
+import Image from 'next/image';
+
 const page = () => {
-  return <div>About Us</div>;
+  return (
+    <main className="flex-1 p-10 bg-gray-100">
+      <h1 className="text-3xl font-bold text-gray-700">O Nas</h1>
+      <div className="bg-white shadow-lg rounded-xl p-6 mt-5">
+        <p className="text-gray-600 leading-relaxed">
+          Jesteśmy zespołem oddanych profesjonalistów, których celem jest zrewolucjonizowanie zarządzania gospodarstwem za pomocą najnowocześniejszych technologii.
+          Nasza platforma pomaga rolnikom optymalizować ich działania, śledzić zapasy i zapewniać dobrostan zwierząt gospodarskich i upraw.
+        </p>
+
+        <div className="mt-6 grid md:grid-cols-3 gap-6">
+          {teamMembers.map((member, index) => (
+            <div key={index} className="bg-gray-50 p-5 rounded-lg shadow text-center">
+              <Image src={member.image} width={120} height={80} className="w-30 h-20 rounded-full mx-auto mb-3" alt={member.name} />
+              <h3 className="text-lg font-semibold">{member.name}</h3>
+              <p className="text-gray-500">{member.role}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </main>
+  );
 };
+
+const teamMembers = [
+  { name: "Grzegorz Banaszak", role: "Założyciel & Główny Programista", image: "/person1.png" },
+  { name: "Patryk Bednarek", role: "Programista", image: "/person2.png" },
+  { name: "Krzysztof Wasiak", role: "Programista", image: "/person3.png" },
+];
 
 export default page;
